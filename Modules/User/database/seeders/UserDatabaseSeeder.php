17ad7691d259682eb104f4bb95107571560bfdf8
EslamTeleb1 <et1136@fayoum.edu.eg>
2025-09-20 21:58:44 +0300

    made a users seeder

--- Modules/User/database/seeders/UserDatabaseSeeder.php
+++ Modules/User/database/seeders/UserDatabaseSeeder.php
@@ -2,15 +2,41 @@
 
 namespace Modules\User\Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class UserDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+   public function run(): void
     {
-        // $this->call([]);
-    }
+        // Create roles if they don't exist
+        $managerRole = Role::firstOrCreate(['name' => 'Manager']);
+        $userRole    = Role::firstOrCreate(['name' => 'User']);
+
+        // Manager user
+        $manager = User::firstOrCreate(
+            ['email' => 'manager@manager.com'],
+            [
+                'name' => 'Manager User',
+                'password' => Hash::make('password123'),  
+            ]
+        );
+        $manager->assignRole($managerRole);
+
+    foreach (range(1, 5) as $i) {
+
+                // Normal user
+                $user = User::firstOrCreate(
+                    ['email' => "user{$i}@user.com"], 
+                    [
+                        'name' => "Normal User {$i}", 
+                        'password' => Hash::make('password123'),
+                    ]
+                );
+
+                $user->assignRole($userRole);
+            }
+     }
 }
